b26127453658e20e9093f322db5bdd913c70e7a8
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-26 20:47:14 +0530

    Create method to add new student details

--- src/main/java/com/mahezh/classmanagement/controller/ClassController.java
+++ src/main/java/com/mahezh/classmanagement/controller/ClassController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mahezh.classmanagement.model.ClassM;
 import com.mahezh.classmanagement.service.ClassService;
 
 @Controller
@@ -20,6 +21,14 @@ public class ClassController {
 	public String listStudentDetails(Model model) {
 		model.addAttribute("student_details", classService.getAllDetails());
 		return "student_details";
+	}
+	
+	@GetMapping("/student/new")
+	public String createStudentDetailForm(Model model) {
+		
+		ClassM classm = new ClassM();
+		model.addAttribute("classm", classm);
+		return "create_new_student";
 	}
 
 }
